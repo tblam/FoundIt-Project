07b5675f316f91a00400e14d61a7b3c6ffd3eea9
LYDERIC <lydericmuys@gmail.com>
2016-05-11 22:18:32 -0700

    Fixed another merge conflict.

--- forum.php
+++ forum.php
@@ -19,19 +19,7 @@ include('php/signup.php');
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<<<<<<< HEAD
-    <style>
-	  .carousel-inner > .item > img,
-	  .carousel-inner > .item > a > img {
-		  width: 80%;
-		  margin: 0 auto;
-	  }
-	  </style>
-
-=======
     <script src="js/jquery.js"></script>
-    
->>>>>>> lyderic
     <!-- Custom CSS -->
     <link href="css/forum.css" rel="stylesheet">
     
